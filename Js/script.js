4fc4d5520c66dec321a30bd35d9621da73d6eebc
unknown <gausulislam5@gmail.com>
2023-09-17 20:37:43 +0600

    update js

--- Js/script.js
+++ Js/script.js
@@ -4,3 +4,20 @@ console.log(h2List);
 for (const h2 of h2List) {
     h2.style.backgroundColor='lightblue';
 }
+
+const backPack=document.getElementById('backpack');
+backPack.style.backgroundColor='tomato';
+
+const cardList=document.getElementsByClassName('card');
+for (const card of cardList) {
+    card.style.borderRadius='30px'; 
+}
+
+const buttonUpdate=document.getElementById('btn-more1');
+buttonUpdate.addEventListener('click',function(){
+    console.log('you have clicked');
+})
+
+document.getElementById('btn-more1').addEventListener('click',function(){
+    console.log('button clicked');
+})
